7d0f886c2969385d075b3aa024eecc37e16e558e
waldika13 <61677015+waldika13@users.noreply.github.com>
2020-10-11 11:34:18 +0700

    Create javaMethodOverloading.java

---
+++ pertemuan4/src/com/pbo/javaMethodOverloading.java
@@ -0,0 +1,18 @@
+package com.pbo;
+
+public class javaMethodOverloading {
+    static int plusMethodInt(int x, int y) {
+        return x + y;
+    }
+
+    static double plusMethodDouble(double x, double y) {
+        return x - y;
+    }
+
+    public static void main (String[] args) {
+        int myNum1 = plusMethodInt(8, 5);
+        double myNum2 = plusMethodDouble(8.3, 6.26);
+        System.out.println("int : " + myNum1);
+        System.out.println("Double : " + myNum2);
+    }
+}
