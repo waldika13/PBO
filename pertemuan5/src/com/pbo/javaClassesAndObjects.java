be68dd5e0d9f2666cff9457f59e4e6ffa43fd151
waldika13 <61677015+waldika13@users.noreply.github.com>
2020-10-18 16:03:35 +0700

    Create javaClassesAndObjects.java

---
+++ pertemuan5/src/com/pbo/javaClassesAndObjects.java
@@ -0,0 +1,14 @@
+package com.pbo;
+
+public class javaClassesAndObjects {
+
+    int x = 5;
+
+    public static void main(String[] args){
+
+        javaClassesAndObjects myObj1 = new javaClassesAndObjects(); //Object 1
+        javaClassesAndObjects myObj2 = new javaClassesAndObjects(); //Object 2
+        System.out.println(myObj1.x);
+        System.out.println(myObj2.x);
+    }
+}
